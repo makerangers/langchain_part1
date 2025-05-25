b88d11be2ba249ca199506d5c2dccbd73f2afa10
makerangers <154067926+makerangers@users.noreply.github.com>
2025-05-25 14:43:40 +0900

    Add files via upload

---
+++ search.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+import meilisearch
+load_dotenv()
+
+client = meilisearch.Client(url = os.getenv('MEILISEARCH_URL'), api_key=os.getenv('MEILISEARCH_API_KEY'))
+
+def stock_search(query):
+    client.index('nasdaq').search(query)
